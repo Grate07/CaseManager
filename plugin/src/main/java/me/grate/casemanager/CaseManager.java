4f92e645388750c476d406a361f1d8d700968576
Grate <grate00712@gmail.com>
2026-09-22 21:06:45 +0530

    Refactor CaseManager initialization and command setup

--- plugin/src/main/java/me/grate/casemanager/CaseManager.java
+++ plugin/src/main/java/me/grate/casemanager/CaseManager.java
@@ -1,6 +1,7 @@
 package me.grate.casemanager;
 
 import me.grate.casemanager.casefile.CaseService;
+import me.grate.casemanager.command.CaseCommand;
 import me.grate.casemanager.database.DatabaseManager;
 import me.grate.casemanager.database.DatabaseTables;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +15,7 @@ public final class CaseManager extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         instance = this;
 
         saveDefaultConfig();
@@ -23,29 +25,72 @@ public final class CaseManager extends JavaPlugin {
         databaseManager = new DatabaseManager(this);
 
         try {
-            databaseManager.connect();
-            DatabaseTables.createTables(this, databaseManager);
 
-            caseService = new CaseService(databaseManager);
+            databaseManager.connect();
+
+            DatabaseTables.createTables(
+                    this,
+                    databaseManager
+            );
+
+            caseService = new CaseService(
+                    databaseManager
+            );
 
         } catch (Exception exception) {
-            getLogger().severe("Unable to initialize the database.");
+
+            getLogger().severe(
+                    "Unable to initialize the database."
+            );
+
             exception.printStackTrace();
 
-            getServer().getPluginManager().disablePlugin(this);
+            getServer()
+                    .getPluginManager()
+                    .disablePlugin(this);
+
             return;
         }
 
-        getLogger().info("CaseManager enabled!");
+        CaseCommand caseCommand =
+                new CaseCommand(this);
+
+        if (getCommand("case") != null) {
+
+            getCommand("case")
+                    .setExecutor(caseCommand);
+
+            getCommand("case")
+                    .setTabCompleter(caseCommand);
+
+        } else {
+
+            getLogger().severe(
+                    "The /case command is missing from plugin.yml!"
+            );
+
+            getServer()
+                    .getPluginManager()
+                    .disablePlugin(this);
+
+            return;
+        }
+
+        getLogger().info(
+                "CaseManager enabled!"
+        );
     }
 
     @Override
     public void onDisable() {
+
         if (databaseManager != null) {
             databaseManager.close();
         }
 
-        getLogger().info("CaseManager disabled!");
+        getLogger().info(
+                "CaseManager disabled!"
+        );
     }
 
     public static CaseManager getInstance() {
